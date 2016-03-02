bdd8acccd44455d4e230e5af42a81e7272fa88c7
Ugghhh <angelowo@umich.edu>
2016-03-01 20:28:55 -0800

    some changes

--- CalculatorBrain.swift
+++ CalculatorBrain.swift
@@ -57,6 +57,28 @@ class CalculatorBrain {
         learnOp(Op.PiOperation("π"))
     }
     
+    //pass back and forth the program operation stack
+    typealias PropertyList = AnyObject
+    
+    var program: PropertyList { //guaranteed to be a PropertyList
+        get {
+            return opStack.map{ $0.description }
+        }
+        set {
+            if let opSymbols = newValue as? Array<String> {
+                var newOpStack = [Op]()
+                for opSymbol in opSymbols {
+                    if let op = knownOps[opSymbol] {
+                        newOpStack.append(op)
+                    } else if let operand = NSNumberFormatter().numberFromString(opSymbol)?.doubleValue {
+                        newOpStack.append(.Operand(operand))
+                    }
+                }
+                opStack = newOpStack
+            }
+        }
+    }
+    
     private func evaluate(ops: [Op]) -> (result: Double?, remainingOps: [Op], opHistory: String) {
         if !ops.isEmpty {
             var remainingOps = ops
